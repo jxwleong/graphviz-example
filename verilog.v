6d956d8c1263ee1c831a073cdfe12fdd54afbf26
jxwleong <jason.xie.wei.leong@gmail.com>
2025-03-30 12:58:47 +0100

    Added sample verilog.v to test out the parser

--- verilog.v
+++ verilog.v
@@ -1,7 +1,8 @@
 module and_gate (
     input a, // 4-bit input 
     input b, // 8-bit input 
-    output c // 12-bit output ); 
+    output c // 12-bit output 
+); 
     
     assign c = a & b; 
 endmodule
