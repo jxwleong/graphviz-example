b85143bd8b5fb2981576506482909c72f3b8b089
jxwleong <jason.xie.wei.leong@gmail.com>
2025-05-11 16:23:51 +0100

    Updated perl script to read verilog files of multiple path

--- src/test_perl_verilog_netlist.pl
+++ src/test_perl_verilog_netlist.pl
@@ -24,12 +24,13 @@ use Data::Dumper; # To print out the hash or array
 my @rtl_dirs = (
     "/home/jason/graphviz-example/examples/altera-de1-processor/rtl/",
     "/home/jason/graphviz-example/examples/altera-de1-processor/rtl/archive",
-    "/home/jason/graphviz-example/examples/riscv_cpu_example"
+    "/home/jason/graphviz-example/examples/riscv_cpu_example",
+    "/home/jason/graphviz-example/examples/SimpleCPU/mips/mips-pipeline/verilog"
     # Add more directories as needed
 );
-my $top_module_file = '/home/jason/graphviz-example/examples/riscv_cpu_example/CPU.v';
+my $top_module_file = '/home/jason/graphviz-example/examples/SimpleCPU/mips/mips-pipeline/verilog/top.v';
 
-my @verilog_files = map { glob("$_*.v $_*.sv") } @rtl_dirs;
+my @verilog_files = map { glob("$_/*.v $_/*.sv") } @rtl_dirs;
 
 
 
@@ -39,6 +40,9 @@ my @verilog_files = map { glob("$_*.v $_*.sv") } @rtl_dirs;
     && $_ !~ /VCPU\.v$/ 
     && $_ !~ /WIN\.v$/  
     && $_ !~ /WIN_syn\.v$/  
+    #&& $_ !~ /riscv_xilinx_2r1w\.v$/  
+    #&& $_ !~ /riscv_regfile\.v$/  
+
 
 } @verilog_files;
 
@@ -49,6 +53,7 @@ foreach my $file (@verilog_files) {
 my $opt = new Verilog::Getopt;
 $opt->parameter(
     "+incdir+$rtl_dirs[2]",
+    "+incdir+/home/jason/graphviz-example/examples/SimpleCPU/mips/mips-pipeline",
     #"-y/home/jason/graphviz-example/examples/altera-de1-processor/rtl",
     #"-y/home/jason/graphviz-example/examples/altera-de1-processor/rtl/archieve",
 );
@@ -99,7 +104,7 @@ my @modules_array;
 my @cells = $top_mod->cells_sorted;
 if (!@cells) {
     print <<"END_MESSAGE";
-Top module ($file) has no submodule instantiations (standalone).
+Top module ($top_module_file) has no submodule instantiations (standalone).
 This parsing is currently not supported.
 Parsing is skipped.
 END_MESSAGE
@@ -154,68 +159,6 @@ foreach my $cell ($top_mod->cells_sorted) {
     push @modules_array, \%ordered_data;
 }
 
-=begin comment
-# Building the instance mapping of the instantiated instance in the module
-my @modules_array;
-
-foreach my $mod ($nl->modules_sorted_level) {
-    my $module_name = $mod->name;
-
-    foreach my $cell ($mod->cells_sorted) {
-        my @inputs;
-        my @outputs;
-        my %cell_data;
-        my $inst_name = $cell->name;
-        my $submod    = $cell->submodname;  # The instantiated module type 
-        my $mod_name  = $cell->module->name;
-        print "Module: $mod_name\n";
-        print "  Instance: $inst_name ($submod)\n";
-
-        # Skip auto-generated unnamed instances
-        next if $inst_name =~ /^__unnamed_instance/;
-
-        $cell_data{"instance"} = $inst_name;
-        $cell_data{"instance_module"} = $submod;
-        foreach my $pin ($cell->pins_sorted) {
-            my %pin_data;
-            
-            my $pin_name = $pin->name;        # Port name of submodule
-            my $net_name  = $pin->netname;     # The signal connected to it
-            my $port     = $pin->port;
-
-            my $port_name = $port->name;
-            my $pin_direction = $port->direction;
-
-            $pin_data{"pin"} = $pin_name;
-            $pin_data{"net"} = $net_name;
-            while (my ($key, $value) = each %pin_data) {
-                print "$key: $value\n";
-            }
-
-            # \%pin_data the "\" is to keep the actual hash
-            # otherwise it will break the element down and become array instead
-            if ($pin_direction eq 'in') {
-                push @inputs,  \%pin_data;
-            } elsif ($pin_direction eq 'out') {
-                push @outputs, \%pin_data;
-            }
-        }
-        # Tie an ordered hash for clean output
-        tie my %ordered_data, 'Tie::IxHash';
-        %ordered_data = (
-            instance => $inst_name,
-            instance_module => $submod,
-            input  => \@inputs,
-            output => \@outputs
-        );
-
-        # Push the hashref into the array
-        push @modules_array, \%ordered_data;
-
-    }
-
-}
-=cut
 
 # Mapping out the connections of the pins of each instance
 # First we create a mapping of output pins associated to each instances
