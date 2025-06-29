28cd5fb0aa7864726c141977b647562de4bbfb72
jxwleong <jason.xie.wei.leong@gmail.com>
2025-06-29 10:21:37 +0100

    Update the test.sh to use dynamic path instead of hard-coded path in my environment

--- src/test.sh
+++ src/test.sh
@@ -1,1 +1,12 @@
-perl test_perl_verilog_netlist.pl  -f /home/jason/graphviz-example/examples/riscv_cpu_example/riscv.vc +define+FOO=bar
+#!/bin/bash
+
+this_script_path=$(readlink -f "$0")
+
+levels_up=2
+root_dir="$this_script_path"
+for ((i = 0; i < levels_up; i++)); do
+    root_dir=$(dirname "$root_dir")
+done
+
+
+perl $root_dir/src/test_perl_verilog_netlist.pl  -f $root_dir/examples/riscv_cpu_example/riscv.vc +define+FOO=bar
