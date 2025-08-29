60880fc41b964927c639fb9e46890acd4b29251a
jxwleong <jason.xie.wei.leong@gmail.com>
2025-08-29 19:05:53 +0100

    Update test.sh to invoke the python script to convert parsed verilog to graphviz diagram in one full flow

--- src/test.sh
+++ src/test.sh
@@ -10,3 +10,4 @@ done
 
 
 perl $root_dir/src/test_perl_verilog_netlist.pl  -top /home/jason/graphviz-example/examples/riscv_cpu_example/CPU.v -f $root_dir/examples/riscv_cpu_example/riscv.vc +define+FOO=bar
+python3 $root_dir/src/test_graphviz.py --input_file $root_dir/src/CPU.json
