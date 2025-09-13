8309b8f4e80d96df5cdb1005ef70a670fb94750c
jxwleong <jason.xie.wei.leong@gmail.com>
2025-09-13 09:35:32 +0100

    Updated the full path in the test.sh script to relative path

--- src/test.sh
+++ src/test.sh
@@ -9,5 +9,5 @@ for ((i = 0; i < levels_up; i++)); do
 done
 
 
-perl $root_dir/src/test_perl_verilog_netlist.pl  -top /home/jason/graphviz-example/examples/riscv_cpu_example/CPU.v -f $root_dir/examples/riscv_cpu_example/riscv.vc +define+FOO=bar
+perl $root_dir/src/test_perl_verilog_netlist.pl  -top $root_dir/examples/riscv_cpu_example/CPU.v -f $root_dir/examples/riscv_cpu_example/riscv.vc +define+FOO=bar
 python3 $root_dir/src/test_graphviz.py --input_file $root_dir/src/CPU.json
